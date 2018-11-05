4de7a4b627468fd140672e76fc144e8333697359
YuCheol <cokr3430@gmail.com>
2018-11-05 09:58:08 +0900

    Apply react-loadable

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
+import Loadable from 'react-loadable';
+import Loading from './Loading';
+
+const Greeter = Loadable({
+  loader: () => import('./Greeter'),
+  loading: Loading,
+})
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      show: false,
+    }
+  }
+
   render() {
+    const { show } = this.state;
+
     return (
       <div>
         <h1>SSR, Code Spliting Example</h1>
+        <button onClick={() => this.setState(({show}) => ({show: !show}))}>Toggle</button>
+        {show && (
+          <Greeter/>
+        )}
       </div>
     );
   }
---
+++ src/Greeter.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Greeter = () => (
+  <h2>hi</h2>
+);
+
+export default Greeter;
